ac5c8e5e9423b6ee5bbbfe149a8f8da294cbf325
JanPerez35 <jan.perez21@upr.edu>
2025-03-25 16:39:15 -0400

    Changed some labels in the 8 queens puzzle

--- hill_climbing_steepest_ascent_solve_8_queens_puzzle.py
+++ hill_climbing_steepest_ascent_solve_8_queens_puzzle.py
@@ -180,9 +180,9 @@ fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
 # Plot 1: Search Cost vs. Attacking Pairs Costs
 axs[0].scatter(optimal_solution_costs, search_costs, color='green', alpha=0.7, label="Search Cost")
-axs[0].set_xlabel("Number of Attacking Pairs (Initial State)")
+axs[0].set_xlabel("Optimal Cost")
 axs[0].set_ylabel("Search Cost (Steps Taken)")
-axs[0].set_title("Search Cost vs. Number of Attacking Pairs")
+axs[0].set_title("Search Cost vs. Optimal Cost")
 axs[0].legend()
 axs[0].grid(True)
 
