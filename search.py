200671b1d439dddacf513bc205433b39af25f45b
ChristopherMayens <christopher.mayens@upr.edu>
2025-03-26 23:04:39 -0400

    Edited Simulated Annealing

--- search.py
+++ search.py
@@ -726,18 +726,18 @@ def simulated_annealing(problem, schedule=exp_schedule()):
     """[Figure 4.5] CAUTION: This differs from the pseudocode as it
     returns a state instead of a Node."""
     current = Node(problem.initial)
+
     for t in range(sys.maxsize):
         T = schedule(t)
         if T == 0:
-            return current.state
+            return current #Edited to return current instead of current.state
         neighbors = current.expand(problem)
         if not neighbors:
-            return current.state
+            return current #Edited to return current instead of current.state
         next_choice = random.choice(neighbors)
-        delta_e = problem.value(next_choice.state) - problem.value(current.state)
+        delta_e = problem.value(next_choice.state) - problem.value(current.state) 
         if delta_e > 0 or probability(np.exp(delta_e / T)):
-            current = next_choice
-
+            current = next_choice               
 
 def simulated_annealing_full(problem, schedule=exp_schedule()):
     """ This version returns all the states encountered in reaching 
@@ -748,14 +748,16 @@ def simulated_annealing_full(problem, schedule=exp_schedule()):
         states.append(current.state)
         T = schedule(t)
         if T == 0:
-            return states
+            return states, len(states)  # Edited to return path and search cost
         neighbors = current.expand(problem)
         if not neighbors:
-            return current.state
+            return states, len(states)  # Edited to return path and search cost
         next_choice = random.choice(neighbors)
-        delta_e = problem.value(next_choice.state) - problem.value(current.state)
+        delta_e = problem.h(next_choice) - problem.h(current) #problem.values() replaced with problem.h() and eliminated .state
         if delta_e > 0 or probability(np.exp(delta_e / T)):
             current = next_choice
+
+    return states, len(states) # Edited to return path and search cost
 
 
 def and_or_graph_search(problem):
